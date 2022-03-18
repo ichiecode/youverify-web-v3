cd544e5f17055e1839e29b0313744c3756792890
Chinedu Ohagwu <chinedu@youverify.co>
2022-03-18 10:37:22 +0100

    Implement subscriber

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,28 +1,49 @@
 <template>
   <div>
-	  <section class="md:pb-20 pb-5">
-		<div class="md:flex items-center gap-2 "> 
-      <div class="w-full md:w-1/2">
-        <h3>Newsletter</h3>
-        <p>Subscribe to get information about our product and coupons</p>
+    <section class="md:pb-20 pb-5">
+      <div class="md:flex items-center gap-2">
+        <div class="w-full md:w-1/2">
+          <h3>Newsletter</h3>
+          <p>Subscribe to get information about our product and coupons</p>
+        </div>
+        <div class="w-full md:w-1/2">
+          <form class="my-4 md:my-0 flex">
+            <input
+              class="w-full p-2 rounded-l-lg outline-none border-t mr-0 border-b outline text-gray-800 bg-white"
+              placeholder="Email address"
+              v-model="email"
+            />
+            <button
+              @click.prevent="subscriber"
+              class="px-4 rounded-r-lg  inline-flex items-center bg-blue text-white font-bold border-t border-b border-r"
+            >
+
+              Subscribe
+              <svg
+                v-if="loading"
+                role="status"
+                class="inline mr-3 w-4 h-4 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </form>
+          <pre class="text-sm mt-1 text-red-400">{{error}}</pre>
+          <pre class="text-sm mt-1 text-green-400">{{message}}</pre>
+        </div>
       </div>
-      <div class="w-full md:w-1/2">
-        <form class="my-4 md:my-0 flex ">
-          <input
-            class="w-full p-2 rounded-l-lg outline-none border-t mr-0 border-b outline text-gray-800  bg-white"
-            placeholder="Email address"
-          />
-          <button
-            class="px-4 rounded-r-lg bg-blue text-white font-bold border-t border-b border-r"
-          >
-            Subscribe
-          </button>
-        </form>
-      </div>
-    </div>
-	</section>
+    </section>
     <div class="flex pb-16 sm:flex-nowrap flex-wrap">
-		
       <section class="sm:w-4/12 w-full sm:mb-0 mb-12">
         <div class="">
           <p class="mb-8">
@@ -134,7 +155,7 @@
         </div>
       </section>
     </div>
-    
+
     <hr class="border-grey sm:mb-0 mb-8" />
 
     <div
@@ -191,11 +212,35 @@
 import { mapState } from "vuex";
 export default {
   name: "footerComponent",
+  data() {
+    return {
+      email: "",
+      error: "",
+      message: ""
+    };
+  },
   methods: {
     async getAllIndustries() {
       const allIndustries = await this.$store.dispatch(
         "industries/getIndustries"
       );
+    },
+    async subscriber() {
+      const payload = {
+        email: this.email,
+      };
+      const response = await this.$store.dispatch(
+        "subscriber/subscribe",
+        payload
+      );
+      if (response.error) {
+        this.error = response.error
+        this.message = ""
+      } else {
+        this.email = ""
+        this.error = ""
+        this.message = response.message
+      }
     },
   },
   mounted() {
@@ -204,6 +249,7 @@ export default {
   computed: {
     ...mapState({
       industries: (state) => state.industries.industries,
+      loading: (state) => state.subscriber.loading
     }),
   },
 };
