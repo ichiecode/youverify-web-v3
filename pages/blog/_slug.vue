82b2e92b54914cbf2660eb9b84808914a6ecf1b0
Chinedu Ohagwu <chinedu@youverify.co>
2022-10-12 11:23:27 +0100

    Webpage Optimization

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -33,8 +33,10 @@
             class="w-full object-cover object-center rounded-xl block mt-10 mb-20"
           >
             <img
-              :src="formattedPost.image ? formattedPost.image.url : ''"
+              :src="formattedPost.image ? formattedPost.image.formats.large.url : ''"
               class="w-full"
+              :height="formattedPost.image.height"
+              :width="formattedPost.image.width"
               :alt="formattedPost.image ? formattedPost.image.caption : ''"
             />
           </div>
