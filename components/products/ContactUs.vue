79fc6e587912dcbe814d7fb16449f09db2ea67d6
Jesutooni <edeworuyoyou@gmail.com>
2023-02-07 05:57:22 +0100

    reusable product components

---
+++ components/products/ContactUs.vue
@@ -0,0 +1,57 @@
+<template>
+  <section class="py-20 bg-blue-100">
+    <div class="px-4 md:px-8 max-w-5xl mx-auto text-center">
+      <h1 class="text-xl md:text-3xl font-bold">{{ heading }}</h1>
+      <p class="text-xl font-semibold mt-12">
+        {{ note }}
+      </p>
+      <form class="form-group w-full mt-7 md:mt-16 max-w-md mx-auto">
+        <div class="space-y-4">
+          <input
+            class="py-3.5 px-6 w-full rounded"
+            type="text"
+            placeholder="Type your name"
+          />
+          <input
+            class="py-3.5 px-6 w-full rounded"
+            type="text"
+            placeholder="Company name"
+          />
+          <input
+            class="py-3.5 px-6 w-full rounded"
+            type="email"
+            placeholder="Type your corporate email address"
+          />
+          <input
+            class="py-3.5 px-6 w-full rounded"
+            type="tel"
+            placeholder="+234"
+          />
+        </div>
+        <AppButton
+          buttonText="Get a free demo"
+          buttoncolor="bluebutton"
+          class="w-full mt-14"
+        />
+        <p class="text-xs text-left mt-3">
+          By clicking the button you agree with our
+          <nuxt-link
+            to="/privacy-policy"
+            class="text-blue cursor-pointer font-semibold underline"
+          >
+            Privacy Policy</nuxt-link
+          >
+        </p>
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: String,
+    note: String,
+  },
+};
+</script>
---
+++ components/products/Offers.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="pt-24">
+    <h1
+      class="text-3xl md:text-5xl font-semibold text-blue-300 text-center max-w-4xl mx-auto capitalize"
+    >
+      {{ title }}
+    </h1>
+
+    <div class="mt-9">
+      <div
+        v-for="(offer, index) in offers"
+        :key="index"
+        class="py-28"
+        :class="index % 2 !== 0 ? 'bg-blue-100' : 'bg-white'"
+      >
+        <div
+          class="flex flex-col justify-between items-center gap-12 px-4 md:px-8 max-w-screen-xl mx-auto"
+          :class="index % 2 !== 0 ? 'md:flex-row' : 'md:flex-row-reverse'"
+        >
+          <div class="w-full">
+            <LazyImage :class="index % 2 == 0 ? 'mx-auto md:ml-auto': 'mx-auto md:mr-auto'"
+              :src="
+                require(`~/assets/images/illustrations/${offer.illustration}.svg`)
+              "
+            />
+          </div>
+          <div class="w-full">
+            <h1 class="capitalize font-semibold text-3xl">
+              {{ offer.title }}
+            </h1>
+            <p class="mt-3 md:mt-7">
+              {{ offer.note }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    offers: {},
+  },
+};
+</script>
